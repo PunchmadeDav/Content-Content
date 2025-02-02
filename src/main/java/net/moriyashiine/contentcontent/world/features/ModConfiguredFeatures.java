3da0f9adceefc362a9d1924dc903fa6f8417139e
grape-lol <165611987+grape-lol@users.noreply.github.com>
2025-02-01 21:51:59 -0600

    adsads

--- src/main/java/net/moriyashiine/contentcontent/world/features/ModConfiguredFeatures.java
+++ src/main/java/net/moriyashiine/contentcontent/world/features/ModConfiguredFeatures.java
@@ -1,8 +1,6 @@
 package net.moriyashiine.contentcontent.world.features;
 
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.block.Blocks;
-import net.minecraft.data.DataProvider;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
@@ -11,13 +9,12 @@ import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
-import net.minecraft.world.gen.feature.util.BlockMatchRuleTest;
+import net.moriyashiine.contentcontent.ContentContent;
 import net.moriyashiine.contentcontent.block.ModBlocks;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
-public class ModConfiguredFeatures implements DataProvider {
+public class ModConfiguredFeatures {
    public static final List<OreFeatureConfig.Target> ANCIENT_VASES = List.of(
            OreFeatureConfig.createTarget(new BlockMatchRuleTest(Blocks.DEEPSLATE_BRICKS), ModBlocks.ANCIENT_VASE_1.getDefaultState())
    );
@@ -25,15 +22,7 @@ public class ModConfiguredFeatures implements DataProvider {
            OreFeatureConfig.createTarget(new BlockMatchRuleTest(Blocks.DEEPSLATE_TILES), ModBlocks.ANCIENT_VASE_1.getDefaultState())
    );
 
-   private final FabricDataOutput output;
-   private final CompletableFuture<Registerable<?>> registriesFuture;
-
-   public ModConfiguredFeatures(FabricDataOutput output, CompletableFuture<Registerable<?>> registriesFuture) {
-      this.output = output;
-      this.registriesFuture = registriesFuture;
-   }
-
-   public void run(Registerable<ConfiguredFeature<?, ?>> context) {
+   public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> context) {
       register(context, "ancient_vase_1", Feature.ORE, new OreFeatureConfig(ANCIENT_VASES, 6));
       register(context, "ancient_vase_11", Feature.ORE, new OreFeatureConfig(ANCIENT_VASES1, 6));
    }
@@ -43,8 +32,7 @@ public class ModConfiguredFeatures implements DataProvider {
       context.register(key, new ConfiguredFeature<>(feature, config));
    }
 
-   @Override
-   public String getName() {
-      return "ModConfiguredFeatures";
+   public static void registerConfiguredFeatures() {
+      ContentContent.LOGGER.debug("Registering the ModConfiguredFeatures for contentcontent");
    }
 }
--- src/main/java/net/moriyashiine/contentcontent/world/features/ModPlacedFeatures.java
+++ src/main/java/net/moriyashiine/contentcontent/world/features/ModPlacedFeatures.java
@@ -1,7 +1,5 @@
 package net.moriyashiine.contentcontent.world.features;
 
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.minecraft.data.DataProvider;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
@@ -11,9 +9,8 @@ import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.placementmodifier.*;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
-public class ModPlacedFeatures implements DataProvider {
+public class ModPlacedFeatures {
    public static final RegistryKey<PlacedFeature> ANCIENT_VASE_1_PLACED = RegistryKey.of(
            RegistryKeys.PLACED_FEATURE, new Identifier("contentcontent", "ancient_vase_1_placed")
    );
@@ -22,16 +19,7 @@ public class ModPlacedFeatures implements DataProvider {
            RegistryKeys.PLACED_FEATURE, new Identifier("contentcontent", "ancient_vase_1_placed1")
    );
 
-   private final FabricDataOutput output;
-   private final CompletableFuture<Registerable<?>> registriesFuture;
-
-   public ModPlacedFeatures(FabricDataOutput output, CompletableFuture<Registerable<?>> registriesFuture) {
-      this.output = output;
-      this.registriesFuture = registriesFuture;
-   }
-
-   @Override
-   public void run(Registerable<PlacedFeature> context) {
+   public static void bootstrap(Registerable<PlacedFeature> context) {
       context.register(ANCIENT_VASE_1_PLACED, new PlacedFeature(
               context.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE).getOrThrow(
                       RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, new Identifier("contentcontent", "ancient_vase_1"))
@@ -53,10 +41,5 @@ public class ModPlacedFeatures implements DataProvider {
 
    private static List<PlacementModifier> modifiersWithCount(int count, PlacementModifier heightModifier) {
       return modifiers(CountPlacementModifier.of(count), heightModifier);
-   }
-
-   @Override
-   public String getName() {
-      return "ModPlacedFeatures";
    }
 }
